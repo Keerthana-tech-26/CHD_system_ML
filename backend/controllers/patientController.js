7862338d141fec0399d6d957a1e2bb99dd408421
Keerthana Motte <kemo22aiml@cmrit.ac.in>
2025-09-01 19:35:08 +0530

    Delete backend directory

--- backend/controllers/patientController.js
+++
@@ -1,6 +0,0 @@
-exports.getAllPatients = (req, res) => {
-  res.json([
-    { id: 1, name: "John Doe", age: 54 },
-    { id: 2, name: "Jane Smith", age: 63 }
-  ]);
-};
--- backend/routes/patients.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllPatients } = require("../controllers/patientController.js");
-
-router.get("/", getAllPatients);
-
-module.exports = router;
